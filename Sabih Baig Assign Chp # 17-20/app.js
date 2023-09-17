9acfe80245fa40913893e9ffc529c56ff3b9bac4
SabihBaig <143556056+SabihBaig@users.noreply.github.com>
2023-09-18 01:49:48 +0500

    Add files via upload

---
+++ Sabih Baig Assign Chp # 17-20/app.js
@@ -0,0 +1,54 @@
+// Question # 01
+{
+for ( a = 1; a <= 10; a++) {
+    document.write("sabih", "<br>");
+}
+}
+
+// // Question # 02
+for (var a = 1; a <= 11; a++) {
+    let name = "M Sabih";
+
+    document.write("<br>","<br>","M Sabih","<br>");
+} 
+   
+// // Question # 03
+{
+    console.log("(;;) {")
+}
+
+// Question # 04
+for (var x=0; x < 100; x++) {
+    document.write("<br>","<br>","(x = 0; x <= 4; x++) {","<br>")
+}
+
+// Question #  05
+for (var x = 0; x < -3; x--); {
+    document.write("<br>","<br>","x = 0; x > -3; x--","<br>")
+}
+
+// Question # 10
+arruserName = prompt("Enter Your First Name");
+for (var a = 1; a <= 3; a++); {
+    console.log("Correct") 
+}
+
+// // Question # 11
+var matchFound = false;
+for (var i = 0; i<=4; i++ );{
+    if (matchFound === true){
+        matchFound = true;
+        alert("corrected")
+    }
+}
+if (matchFound === false) {
+    alert("its not on the  list")
+
+}
+
+// Question # 8
+var loop = +prompt("Enter number of Element")
+
+for (var a = 0; a <= 10; a++) {
+    console.log(loop + a)
+}
